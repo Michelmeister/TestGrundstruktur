89d09d447267100401cfd2c474024d5f65bc3166
Michel <m-meinert@gmx.net>
2022-08-09 17:28:28 +0200

    main document

--- CONTROL.py
+++ CONTROL.py
@@ -6,10 +6,12 @@
 
 import devices.PVinverter.PVinverter as PV
 import devices.BATinverter.BATinverter as BAT
+import database.database as DB
 
 PVinverter  = PV.WRpv('PV-Inverter','134.169.132.235','502')
 BATinverter = BAT.WRbat('BAT-Inverter','134.169.132.234','502')
+database = DB.Database('Actual Value database')
 
-concurrentthreads = [PVinverter,BATinverter]
+concurrentthreads = [PVinverter,BATinverter,database]
 for threads in concurrentthreads:
     threads.start()
